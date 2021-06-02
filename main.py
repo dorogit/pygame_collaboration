656d98a608359c48de2c1dd11e0f9e4706866a25
ishaanjainlock10 <84321539+ishaanjainlock10@users.noreply.github.com>
2021-06-02 19:59:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
+pygame.init()
 
 #setting up the screen etc.
 screen = pygame.display.set_mode((1000,750))
 pygame.display.set_caption('HangMan')
 
-pygame.draw.rect(screen, (200,200,200),pygame.Rect(300,350,350,200))
+gallow = pygame.image.load('gallow.png')
+screen.fill((200,200,200))
+Clock = pygame.time.Clock()
 pygame.display.update()
 
 run= True
 
 while run:
-  pressed= pygame.key.get_pressed()
+  event = pygame.event.wait()
+  pressed = pygame.key.get_pressed()
   if pressed[pygame.K_ESCAPE]:
     break
   pygame.display.update()
-
+  Clock.tick(30)
+  screen.blit(gallow,((0,0)))
